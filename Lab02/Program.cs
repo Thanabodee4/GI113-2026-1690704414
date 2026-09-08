78ee2e547a9ce1c04ed2567dabdb5b35035f16e1
Thanabodee4 <Thanabodee.Than@bumail.net>
2026-09-08 16:17:57 +0700

    Lab02 Boss

---
+++ Lab02/Program.cs
@@ -0,0 +1,299 @@
+﻿/*
+ * Student ID : 1690704414
+ * Name       :ธนบดี ธัญญาภิวัฒนา
+ * Section    :129D
+ * No.        :-
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bossName = "Kirin";
+
+            char rank = 'S';
+
+            int level = 7;
+
+            int maxHp = 240;
+
+            int currentHp = 175;
+
+            float attackPower = 42.5f;
+
+            double critMultiplier = 1.75;
+
+            bool isBoss = true;
+
+            // Lab02 Part A
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"Name: {bossName}");
+
+            Console.WriteLine($"Rank: {rank}");
+
+            Console.WriteLine($"Level: {level}");
+
+            Console.WriteLine($"Max HP: {maxHp}");
+
+            Console.WriteLine($"Current HP: {currentHp}");
+
+            Console.WriteLine($"Attack Power: {attackPower}");
+
+            Console.WriteLine($"Critical Multiplier: {critMultiplier}");
+
+            Console.WriteLine($"Is Boss: {isBoss}");
+
+            Console.WriteLine();
+
+            int hpPercent = currentHp - 60;
+
+            Console.WriteLine($"hp Percent: {hpPercent}%");
+
+            Console.WriteLine();
+
+            currentHp = currentHp - 60;
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"hp: {currentHp} / {maxHp}");
+
+            currentHp = currentHp * 100 / maxHp;
+
+            Console.WriteLine($"hp Percent; {hpPercent}%");
+
+            Console.WriteLine();
+
+            // Lab02 Part B
+
+            string bossName3 = "Gram";
+
+            char rank3 = 'S';
+
+            int level3 = 100;
+
+            int maxHp3 = 340;
+
+            int currentHp3 = 275;
+
+            float attackPower3 = 20.5f;
+
+            double critMultiplier3 = 1.75;
+
+            bool isBoss3 = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"Name: {bossName3}");
+
+            Console.WriteLine($"Rank: {rank3}");
+
+            Console.WriteLine($"Level: {level3}");
+
+            Console.WriteLine($"Max HP: {maxHp3}");
+
+            Console.WriteLine($"Current HP: {currentHp3}");
+
+            Console.WriteLine($"Attack Power: {attackPower3}");
+
+            Console.WriteLine($"Critical Multiplier: {critMultiplier3}");
+
+            Console.WriteLine($"Is Boss: {isBoss3}");
+
+            Console.WriteLine();
+
+            int hpPercent3 = currentHp3 - 60;
+
+            Console.WriteLine($"hp Percent: {hpPercent3}%");
+
+            Console.WriteLine();
+
+            currentHp3 = currentHp3 - 60;
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"hp: {currentHp3} / {maxHp3}");
+
+            currentHp3 = currentHp3 * 100 / maxHp3;
+
+            Console.WriteLine($"hp Percent; {hpPercent3}%");
+
+            Console.WriteLine();
+
+            string bossName4 = "MAXKY";
+
+            char rank4 = 'S';
+
+            int level4 = 110;
+
+            int maxHp4 = 440;
+
+            int currentHp4 = 375;
+
+            float attackPower4 = 20.5f;
+
+            double critMultiplier4 = 1.75;
+
+            bool isBoss4 = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"Name: {bossName4}");
+
+            Console.WriteLine($"Rank: {rank4}");
+
+            Console.WriteLine($"Level: {level4}");
+
+            Console.WriteLine($"Max HP: {maxHp4}");
+
+            Console.WriteLine($"Current HP: {currentHp4}");
+
+            Console.WriteLine($"Attack Power: {attackPower4}");
+
+            Console.WriteLine($"Critical Multiplier: {critMultiplier4}");
+
+            Console.WriteLine($"Is Boss: {isBoss4}");
+
+            Console.WriteLine();
+
+            int hpPercent4 = currentHp4 - 60;
+
+            Console.WriteLine($"hp Percent: {hpPercent4}%");
+
+            Console.WriteLine();
+
+            currentHp4 = currentHp4 - 60;
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"hp: {currentHp4} / {maxHp4}");
+
+            currentHp4 = currentHp4 * 100 / maxHp4;
+
+            Console.WriteLine($"hp Percent; {hpPercent4}%");
+
+            Console.WriteLine();
+
+            string bossName5 = "PANAMA";
+
+            char rank5 = 'S';
+
+            int level5 = 110;
+
+            int maxHp5 = 440;
+
+            int currentHp5 = 375;
+
+            float attackPower5 = 20.5f;
+
+            double critMultiplier5 = 1.75;
+
+            bool isBoss5 = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"Name: {bossName5}");
+
+            Console.WriteLine($"Rank: {rank5}");
+
+            Console.WriteLine($"Level: {level5}");
+
+            Console.WriteLine($"Max HP: {maxHp5}");
+
+            Console.WriteLine($"Current HP: {currentHp5}");
+
+            Console.WriteLine($"Attack Power: {attackPower5}");
+
+            Console.WriteLine($"Critical Multiplier: {critMultiplier5}");
+
+            Console.WriteLine($"Is Boss: {isBoss5}");
+
+            Console.WriteLine();
+
+            int hpPercent5 = currentHp5 - 60;
+
+            Console.WriteLine($"hp Percent: {hpPercent5}%");
+
+            Console.WriteLine();
+
+            currentHp5 = currentHp5 - 60;
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"hp: {currentHp5} / {maxHp5}");
+
+            currentHp5 = currentHp5 * 100 / maxHp5;
+
+            Console.WriteLine($"hp Percent; {hpPercent5}%");
+
+            Console.WriteLine();
+
+            string bossName6 = "OWen";
+
+            char rank6 = 'S';
+
+            int level6 = 999;
+
+            int maxHp6 = 540;
+
+            int currentHp6 = 475;
+
+            float attackPower6 = 20.5f;
+
+            double critMultiplier6 = 1.75;
+
+            bool isBoss6 = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"Name: {bossName6}");
+
+            Console.WriteLine($"Rank: {rank6}");
+
+            Console.WriteLine($"Level: {level6}");
+
+            Console.WriteLine($"Max HP: {maxHp6}");
+
+            Console.WriteLine($"Current HP: {currentHp6}");
+
+            Console.WriteLine($"Attack Power: {attackPower6}");
+
+            Console.WriteLine($"Critical Multiplier: {critMultiplier6}");
+
+            Console.WriteLine($"Is Boss: {isBoss6}");
+
+            Console.WriteLine();
+
+            int hpPercent6 = currentHp6 - 60;
+
+            Console.WriteLine($"hp Percent: {hpPercent6}%");
+
+            Console.WriteLine();
+
+            currentHp6 = currentHp6 - 60;
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+
+            Console.WriteLine($"hp: {currentHp6} / {maxHp6}");
+
+            currentHp6 = currentHp6 * 100 / maxHp6;
+
+            Console.WriteLine($"hp Percent; {hpPercent6}%");
+
+            Console.WriteLine();
+        }
+    }
+}
